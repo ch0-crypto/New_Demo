b914a0b9d8a520b0b66cc6db0a0b159d964407f0
emeka.chidiebere <carmie_chris@yahoo.com>
2022-02-23 23:50:58 +0100

    media commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// "use strict";
+
+// const textSpace = document.getElementById("messagee");
+// const submit = document.querySelector(".btn-form");
+
+// addEventListener("click", function () {
+//   if (textSpace === "") {
+//     this.alert("No Message");
+//   }
+// });
